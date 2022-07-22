de48c72bff6f49edd5119647fc3e16805a98e698
muralikrishna887 <71436842+muralikrishna887@users.noreply.github.com>
2022-07-22 15:51:43 +0530

    Create String.java

---
+++ String.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class A1{
+  public static void main(String args[])
+{
+   Scanner sc=new Scanner(System.in);
+    System.out.println("Enter  value");
+    int a=sc.nextInt();
+    System.out.println(a);
+    System.out.println("Enter string");
+    String str=sc.next();
+    System.out.println(str);
+     
+  
+     
+  }
+}
